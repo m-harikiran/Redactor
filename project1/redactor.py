9c296c3428a6c05664b11847069df3cff2ebcc3e
Harikiran Madishetti <harikiranmadishetti95@gmail.com>
2021-04-05 17:44:08 -0700

    Added methon updateStatusLog in redactor.py to updated redactions status in log file

--- project1/redactor.py
+++ project1/redactor.py
@@ -14,3 +14,13 @@ def statsFile(args):
     statusLog.write(
         '************ Redaction Status of Documents ************\n')  # Writing a line to file
     statusLog.close()  # Closing the file
+
+
+# Function to update Redaction process and status
+
+
+def updateStatusLog(message, args):
+    statusLog = open(
+        os.getcwd()+"/project_docs/redaction_logs/"+args, 'a')  # Opening a file to append data
+    statusLog.write('{}\n'.format(message))  # Writing a line to file
+    statusLog.close()  # Closing the file

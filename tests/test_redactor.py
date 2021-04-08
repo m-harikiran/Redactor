31ccc2446627a128229bc64f99696fa55a46b6bb
Harikiran Madishetti <harikiranmadishetti95@gmail.com>
2021-04-07 20:54:42 -0700

    Updated requirements.txt

--- tests/test_redactor.py
+++ tests/test_redactor.py
@@ -1,37 +1,6 @@
 import argparse
 import os
-import nltk
-import re
-import glob
-from commonregex import CommonRegex
-from nltk.corpus import wordnet
 from project1 import redactor
-
-
-def argParser():
-    parser = argparse.ArgumentParser()  # Creating an argument parser object
-    # Adding optinal argument --inputs
-    parser.add_argument("--input", type=str, required=True,
-                        nargs="+", action="append")
-    # Adding optinal argument --names
-    parser.add_argument("--names", required=False, action="store_true")
-    # Adding optinal argument --dates
-    parser.add_argument("--dates", required=False, action="store_true")
-    # Adding optinal argument --phones
-    parser.add_argument("--phones", required=False, action="store_true")
-    # Adding optinal argument --genders
-    parser.add_argument("--genders", required=False, action="store_true")
-    # Adding optinal argument --output
-    parser.add_argument("--output", type=str, required=False,
-                        default="redacted_documents")
-    parser.add_argument("--stats", type=str, required=False,
-                        default="stdout")  # Adding optinal argument --stats
-    # Adding optinal argument --concept
-    parser.add_argument("--concept", type=str,
-                        required=False, nargs="+", action="append")
-
-    args = parser.parse_args(
-        "--input hari kiran --input madi --output /hari --concept Jail dog".split())
 
 
 # Testing creation of status file
